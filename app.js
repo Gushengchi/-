49d0e022fc648e1a7053b48455c9d308d72942c0
AE <AE>
2017-11-20 15:01:22 +0800

    第一次上传

---
+++ app.js
@@ -0,0 +1,16 @@
+App({
+  onLaunch:function(options){
+
+  },
+  onShow:function(options){
+
+  },
+  onHide:function(){
+
+  },
+  onError:function(msg){
+
+  },
+  globalData:{},
+  aa:"这是一个定义的全局属性"
+})
